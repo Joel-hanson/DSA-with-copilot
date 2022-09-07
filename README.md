92bed490d0c01af47fa45e0f45f437c9f794c4d6
Joel Hanson <joelhanson025@gmail.com>
2022-09-07 20:09:55 +0530

    Sliding window

---
+++ README.md
@@ -0,0 +1,27 @@
+# Learnings
+
+Top techniques to approach and solve coding interview questions
+
+1. Visualize the problem by drawing it out
+2. Think about how you would solve the problem by hand
+3. Come up with more examples
+4. Break the question down into smaller independent parts
+5. Apply common data structures and algorithms at the problem
+
+How to optimize your approach or solution:
+
+1. Identify the Best Theoretical Time Complexity of the solution
+2. Identify overlapping and repeated computation
+3. Try different data structures
+4. Identify redundant work
+
+How to optimize space complexity
+
+1. Changing data in-place/overwriting input data
+2. Change a data structure
+
+Order in which I learned each DSA section.
+
+Array:
+
+1. [Sliding window](./sliding_window.py)
---
+++ sliding_window.py
@@ -0,0 +1,62 @@
+"""
+> For a sliding window problem first thing we should do is to create a map to store the frequency of each character in the pattern.
+a - 1
+b - 1
+c - 1
+> Then we will have two pointers, one pointer for the beginning of the window and one pointer for the end of the window.
+t1 = 0
+t2 = 0
+> We should have the count variable to keep track of the number of characters matched in the current window.
+count = len(pattern)
+> We should have the left and right index variables to keep track of the minimum window.
+left = 0
+right = len(string)
+> We should havethe minimum length variable to keep track of the minimum length of the window because the minimum length of the window will less than or equal to the length of the string.
+min_length = len(string)
+> A variable to know if the minimum window is found or not.
+found = False
+"""
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        len_of_t = len(t)
+        len_of_s = len(s)
+        if len_of_t > len_of_s:
+            return ""
+        if t == s:
+            return s
+        if len_of_t == 0:
+            return ""
+        pattern_map = {}
+        for char in t:
+            if char not in pattern_map:
+                pattern_map[char] = 0
+            pattern_map[char] += 1
+        t1 = 0
+        t2 = 0
+        count = len_of_t
+        left = 0
+        right = len_of_s
+        min_length = len_of_s
+        found = False
+        while t2 < len_of_s:
+            if s[t2] in pattern_map:
+                if pattern_map[s[t2]] > 0:
+                    count -= 1
+                pattern_map[s[t2]] -= 1
+            while count == 0:
+                found = True
+                if t2 - t1 + 1 < min_length:
+                    min_length = t2 - t1 + 1
+                    left = t1
+                    right = t2
+                if s[t1] in pattern_map:
+                    pattern_map[s[t1]] += 1
+                    if pattern_map[s[t1]] > 0:
+                        count += 1
+                t1 += 1
+            t2 += 1
+        if found:
+            return s[left:right+1]
+        return ""
+
